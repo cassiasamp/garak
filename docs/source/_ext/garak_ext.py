504b3bdc0fe2730ac3170f9a60b1b751d0f145c6
Cássia Sanctos <csamp@ibm.com>
2025-10-14 16:21:46 -0300

    declaring mu before fstring

--- docs/source/_ext/garak_ext.py
+++ docs/source/_ext/garak_ext.py
@@ -25,7 +25,8 @@ class ShowASRDirective(SphinxDirective):
                     probe_ref = f":obj:`~garak.probes.{probe}`"
                     detector_ref = f":obj:`~garak.detectors.{detector}`"
 
-                    rst += f"\n* {probe_ref}: {100*(1-scores['mu']):.1f}% with detector {detector_ref}"
+                    mu = 1 - scores["mu"]
+		    rst += f"\n* {probe_ref}: {100*(mu):.1f}% with detector {detector_ref}"
 
         if rst:
             rst = (
